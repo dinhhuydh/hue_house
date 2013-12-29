9c64fe2b622e6fd032d6b76dbb3cae7bc059ff24
Huy Dinh <dinhhuydh@gmail.com>
2013-12-29 19:33:56 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HueHouse::Application.config.secret_token = '0e14fe8f699ba5f965e54c47095aa0ae1d709e1684846be4409383605c572a8d38e6bd4fd18ee9cbb266978e854fe387a5e86360b22ee23bca017e5054572299'
